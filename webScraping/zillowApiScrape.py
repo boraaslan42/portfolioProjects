44ab60aabe3169884767ffb77c5c9a94f8915dbf
Bora Aslan <boraaslan2018@gmail.com>
2023-08-21 16:56:24 +0300

    added test link

--- webScraping/zillowApiScrape.py
+++ webScraping/zillowApiScrape.py
@@ -5,6 +5,8 @@ import requests
 # All the raw data houses will be stored in this list as dictionary for each house
 list_of_houses = []
 
+##test link 'https://www.zillow.com/fresno-ca/?searchQueryState=%7B"pagination"%3A%7B%7D%2C"mapBounds"%3A%7B"west"%3A-133.49237243636992%2C"east"%3A-112.46454040511992%2C"south"%3A28.37382409248204%2C"north"%3A40.45682514253177%7D%2C"usersSearchTerm"%3A"undefined"%2C"customRegionId"%3A"403c964d6cX1-CR1hydev9d6b7wl_1c5d8q"%2C"mapZoom"%3A6%2C"regionSelection"%3A%5B%7B"regionId"%3A18203%2C"regionType"%3A6%7D%5D%2C"isMapVisible"%3Atrue%2C"filterState"%3A%7B"price"%3A%7B"min"%3A200000%7D%2C"fore"%3A%7B"value"%3Afalse%7D%2C"mp"%3A%7B"min"%3A1061%7D%2C"ah"%3A%7B"value"%3Atrue%7D%2C"sort"%3A%7B"value"%3A"days"%7D%2C"auc"%3A%7B"value"%3Afalse%7D%2C"nc"%3A%7B"value"%3Afalse%7D%7D%2C"isListVisible"%3Atrue%7D'
+
 
 # -test-data-test-data-test-data-test-data-test-data-test-data-test-data-test-data-test-data-test-data
 link = 'https://www.zillow.com/search/GetSearchPageState.htm?'
@@ -16,6 +18,7 @@ params = {
     "searchQueryState": {"pagination": {"currentPage": 1}, "mapBounds": {"west": -122.30005082666425, "east": -117.04309281885175, "south": 36.23111367681714, "north": 37.3964765262027}, "usersSearchTerm": "undefined", "customRegionId": "403c964d6cX1-CR1hydev9d6b7wl_1c5d8q", "mapZoom": 8, "isMapVisible": "true", "filterState": {"price": {"min": 200000}, "isForSaleForeclosure": {"value": "false"}, "monthlyPayment": {"min": 1061}, "isAllHomes": {"value": "true"}, "sortSelection": {"value": "days"}, "isAuction": {"value": "false"}, "isNewConstruction": {"value": "false"}}, "isListVisible": "true"}, "wants": {"cat1": ["listResults", "mapResults"], "cat2": ["total"]}, "requestId": 12
 }
 # -test-data-test-data-test-data-test-data-test-data-test-data-test-data-test-data-test-data-test-data
+
 
 
 def get_houses_perPage(link, params, pageNum, list_of_houses=list_of_houses):
